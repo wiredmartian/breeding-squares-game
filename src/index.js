ca9b85a20093481724c901c374e4f2e7b71fb649
wiredmartian <11290243+wiredmartian@users.noreply.github.com>
2019-06-20 19:40:03 +0200

    pop squares

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const ctx = canvas.getContext('2d');
 resizeCanvas();
 
 let PARENT_HIT = false;
+let ANIMATION_ID = undefined;
 let squares = [];
 
 let colors = ['#57F408', '#08F4A5', '#08CDF4', '#9F33EE', '#0857F4', '#EE3382'];
@@ -30,7 +31,6 @@ function Square(x, y, dx, dy) {
 
     let h = 40, w = 40;
     this.color = colors[Math.floor(Math.random() * colors.length)];
-    console.log(this.x, this.y);
 
     this.draw = function () {
         ctx.fillStyle = this.color;
@@ -47,11 +47,8 @@ function Square(x, y, dx, dy) {
         this.y += this.dy;
 
         if (this.parentHit()) {
-            for(let i = 0; i < squares.length; i++) {
-                setTimeout(function () {
-                    squares.splice(i, 1);
-                }, 500)
-            }
+            stop();
+            this.reset();
         }
 
         mouseCoords.x = undefined;
@@ -63,20 +60,36 @@ function Square(x, y, dx, dy) {
         // distance between mouse and original square
         if (!(mouseCoords.x === undefined && mouseCoords.y === undefined)) {
             if (calculateDistance((mouseCoords.x -10), this.x, (mouseCoords.y -10), this.y) < 30) {
-                console.log('----------------------------------------------');
                 PARENT_HIT = true;
             }
         }
         return PARENT_HIT;
-    }
+    };
+
+    this.reset = function () {
+        let x = squares.length - 1;
+        let interval = setInterval(function () {
+            if (squares.length > 0) {
+                console.log(squares.length);
+                ctx.clearRect(squares[x].x - 5, squares[x].y -5, w + 10, h + 10);
+                squares.splice(x, 1);
+                x--;
+            } else {
+                clearInterval(interval);
+                ctx.clearRect(0, 0, innerWidth, innerHeight);
+            }
+        }, 500);
+    };
+
 }
+
 
 function createParentSquares() {
     for(let i = 0; i < 2; i++) {
         let x = Math.random() * (canvasDimensions - 50);
         let y = Math.random() * (canvasDimensions - 50);
-        let dx = (Math.random() * 15);
-        let dy = (Math.random() * 15);
+        let dx = (Math.random() * 5);
+        let dy = (Math.random() * 5);
         squares.push(new Square(x, y, dx, dy));
     }
 }
@@ -85,22 +98,31 @@ createParentSquares();
 function animate() {
     let x = Math.random() * (canvasDimensions - 50);
     let y = Math.random() * (canvasDimensions - 50);
-    let dx = (Math.random() * 15);
-    let dy = (Math.random() * 15);
-    requestAnimationFrame(animate);
+    let dx = (Math.random() * 5);
+    let dy = (Math.random() * 5);
+    ANIMATION_ID = requestAnimationFrame(animate);
     ctx.clearRect(0, 0, innerWidth, innerHeight);
     for(let i = 0; i < squares.length; i++){
         squares[i].update();
         if (squares.length <= 100) {
             if (Math.floor(squares[0].x) === Math.floor(squares[1].x)) {
                 squares.push(new Square(x, y, dx, dy));
-                updateScore();
+                //updateScore();
                 break;
             }
         }
     }
 }
 animate();
+
+function stop() {
+    cancelAnimationFrame(ANIMATION_ID);
+    PARENT_HIT = false;
+    ANIMATION_ID = undefined;
+}
+function reset() {
+
+}
 
 function calculateDistance(x2, x1, y2, y1) {
     x1 = Math.floor(x1);
@@ -116,7 +138,6 @@ let squareCount = document.querySelector('#squareCount');
 
 function updateScore() {
     squareCount.textContent = squares.length.toString() + " | 100";
-    console.log('update');
 }
 
 window.addEventListener('resize', function () {
