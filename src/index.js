03b40efe6e1c128432988373993b8a6f4723ae97
wiredmartian <11290243+wiredmartian@users.noreply.github.com>
2019-06-21 01:14:32 +0200

    restart gamw

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ resizeCanvas();
 
 let PARENT_HIT = false;
 let ANIMATION_ID = undefined;
-let squares = [];
+let SQUARES = [];
 
 let colors = ['#57F408', '#08F4A5', '#08CDF4', '#9F33EE', '#0857F4', '#EE3382'];
 
@@ -16,10 +16,24 @@ let mouseCoords = {
     y: undefined
 };
 
+// event handlers
 document.addEventListener('mousedown', function (e) {
     mouseCoords.x = e.layerX;
     mouseCoords.y = e.layerY;
 });
+
+document.addEventListener('keydown', function (e) {
+    if (e.key === " " || e.code === "Space") {
+        if (ANIMATION_ID === undefined && SQUARES.length === 0 && PARENT_HIT === false) {
+            restart();
+        }
+    }
+});
+
+window.addEventListener('resize', function () {
+    resizeCanvas();
+});
+
 
 function Square(x, y, dx, dy) {
 
@@ -67,30 +81,39 @@ function Square(x, y, dx, dy) {
     };
 
     this.reset = function () {
-        let x = squares.length - 1;
+        let x = SQUARES.length - 1;
         let interval = setInterval(function () {
-            if (squares.length > 0) {
-                console.log(squares.length);
-                ctx.clearRect(squares[x].x - 5, squares[x].y -5, w + 10, h + 10);
-                squares.splice(x, 1);
+            if (SQUARES.length > 0) {
+                console.log(SQUARES.length);
+                ctx.clearRect(SQUARES[x].x - 5, SQUARES[x].y -5, w + 10, h + 10);
+                SQUARES.splice(x, 1);
                 x--;
             } else {
                 clearInterval(interval);
                 ctx.clearRect(0, 0, innerWidth, innerHeight);
             }
-        }, 500);
+        }, 200);
     };
 
+    this.restart = function () {
+        createParentSquares();
+        animate();
+    }
 }
 
+
+function restart() {
+    createParentSquares();
+    animate();
+}
 
 function createParentSquares() {
     for(let i = 0; i < 2; i++) {
         let x = Math.random() * (canvasDimensions - 50);
         let y = Math.random() * (canvasDimensions - 50);
-        let dx = (Math.random() * 5);
-        let dy = (Math.random() * 5);
-        squares.push(new Square(x, y, dx, dy));
+        let dx = (Math.random() * 10);
+        let dy = (Math.random() * 10);
+        SQUARES.push(new Square(x, y, dx, dy));
     }
 }
 createParentSquares();
@@ -98,15 +121,15 @@ createParentSquares();
 function animate() {
     let x = Math.random() * (canvasDimensions - 50);
     let y = Math.random() * (canvasDimensions - 50);
-    let dx = (Math.random() * 5);
-    let dy = (Math.random() * 5);
+    let dx = (Math.random() * 10);
+    let dy = (Math.random() * 10);
     ANIMATION_ID = requestAnimationFrame(animate);
     ctx.clearRect(0, 0, innerWidth, innerHeight);
-    for(let i = 0; i < squares.length; i++){
-        squares[i].update();
-        if (squares.length <= 100) {
-            if (Math.floor(squares[0].x) === Math.floor(squares[1].x)) {
-                squares.push(new Square(x, y, dx, dy));
+    for(let i = 0; i < SQUARES.length; i++){
+        SQUARES[i].update();
+        if (SQUARES.length <= 100) {
+            if (Math.floor(SQUARES[0].x) === Math.floor(SQUARES[1].x)) {
+                SQUARES.push(new Square(x, y, dx, dy));
                 //updateScore();
                 break;
             }
@@ -120,9 +143,6 @@ function stop() {
     PARENT_HIT = false;
     ANIMATION_ID = undefined;
 }
-function reset() {
-
-}
 
 function calculateDistance(x2, x1, y2, y1) {
     x1 = Math.floor(x1);
@@ -134,15 +154,7 @@ function calculateDistance(x2, x1, y2, y1) {
     let _sum = _x + _y;
     return Math.floor(Math.sqrt(_sum)) || undefined;
 }
-let squareCount = document.querySelector('#squareCount');
 
-function updateScore() {
-    squareCount.textContent = squares.length.toString() + " | 100";
-}
-
-window.addEventListener('resize', function () {
-    resizeCanvas();
-});
 function resizeCanvas() {
     canvas.width = (window.innerHeight - 80);
     canvas.height = (window.innerHeight - 80);
